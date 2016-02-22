499bb1159530a44d43e981146ea271c21d6b7308
hfskappel <hans.fredrik@skappel.no>
2016-02-22 16:51:06 +0100

    Added som policy parameters

--- policy_inputs.py
+++ policy_inputs.py
@@ -7,30 +7,31 @@ import policy_manager
 #policy_list = [9]
 def generate_policies():
     policy001 = policy_manager.Policy()
-    policy001.match(eth_src="aa:b2:44:34:74:24")
-    policy001.action(load_balance=True)
-    policy001.priority(2)
-    #policy_list.append(policy001)
+    policy001.match(ip_src="10.10.10.103")
+    policy001.action(bandwidth_requirement=3)
+    policy001.priority(8)
 
     policy002 = policy_manager.Policy()
-    policy002.match(eth_dst="aa:b2:44:34:74:24")
-    policy002.action(random_routing=True)
-    policy002.priority(20)
-    #policy_list.append(policy002)
+    policy002.match(ip_src="10.10.10.101")
+    policy002.action(load_balance=True)
+    policy002.priority(4)
 
     policy003 = policy_manager.Policy()
     policy003.match(ip_src="10.10.10.101")
-    policy003.action(random_routing=True)
-    policy003.priority(0)
-    #policy_list.append(policy003)
+    policy003.action(bandwidth_requirement=3)
+    policy003.priority(2)
 
     policy004 = policy_manager.Policy()
     policy004.match(ip_src="10.10.10.102")
-    policy004.action(random_routing=True)
-    #policy004.priority(100)
-    #policy_list.append(policy004)
+    policy004.action(bandwidth_requirement=2)
 
-    policy_list = [policy001, policy002, policy003, policy004]
+    policy005 = policy_manager.Policy()
+    policy005.match(ip_src="10.10.10.104")
+    policy005.action(bandwidth_requirement=3)
+    policy005.priority(10)
+
+
+    policy_list = [policy001, policy002, policy003, policy004, policy005]
     return policy_list
 
 
